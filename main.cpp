45527f6fe087748af4239455497c1a7a6c1562bb
PathakGauresh <pathakgauresh50@gmail.com>
2024-04-03 16:00:00 +0530

    added wifi connection

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <DHT.h>
+#include <WiFi.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
+
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
+
+  // connecting to wifi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
