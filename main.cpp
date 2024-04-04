373a15f4d81ba7ed80cd729ad755c2b6c02405f6
PathakGauresh <pathakgauresh50@gmail.com>
2024-04-04 10:00:00 +0530

    added telegram alerts

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <DHT.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
 
 const char* ssid = "YOUR_WIFI_NAME";
 const char* password = "YOUR_WIFI_PASSWORD";
+const char* telegramUsername = "@YOUR_TELEGRAM_USERNAME";
+const char* apiKey = "YOUR_CALLMEBOT_API_KEY";
+
+float minTemp = 20.0;
+float maxTemp = 26.0;
 
 DHT dht(DHTPIN, DHTTYPE);
+
+void sendAlert(String message) {
+  // sending telegram alert when temp is out of range
+  HTTPClient http;
+  String url = "https://api.callmebot.com/text.php?user=" + String(telegramUsername) + "&apikey=" + String(apiKey) + "&text=" + message;
+  http.begin(url);
+  http.GET();
+  http.end();
+}
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
 
-  // connecting to wifi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -21,10 +35,11 @@ void setup() {
     Serial.print(".");
   }
   Serial.println(" Connected!");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  delay(10000);
+
   // asking dht22 for readings
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
@@ -35,5 +50,14 @@ void loop() {
   Serial.print(hum);
   Serial.println("%");
 
-  delay(2000);
+  // check if temp is out of safe range
+  if (temp > maxTemp) {
+    Serial.println("ALERT: Temp too HIGH!");
+    sendAlert("ALERT! Temp too HIGH: " + String(temp) + "C");
+  } else if (temp < minTemp) {
+    Serial.println("ALERT: Temp too LOW!");
+    sendAlert("ALERT! Temp too LOW: " + String(temp) + "C");
+  } else {
+    Serial.println("Temp is safe.");
+  }
 }
